d0aba3f5a00c85a942a86e435d534645ef7c0dba
anilkumar9854 <91942717+anilkumar9854@users.noreply.github.com>
2021-10-08 09:22:15 +0530

    Update demo.js

--- src/demo.js
+++ src/demo.js
@@ -5,6 +5,9 @@ import React, {
 import Paper from '@material-ui/core/Paper';
 import './table.css';
 import {
+  Getter,
+} from '@devexpress/dx-react-core';
+import {
   SelectionState,
   IntegratedSelection,
   PagingState,
@@ -111,44 +114,14 @@ const selectAllBoxes = (event) => {
   
 }
 export default () => {
-  const [rows, setRows] = useState(
-    [
-      {"id":0, "parentId":null ,"name":"Assets", "account":"" ,"checkbox":<input className="selection" id="0" type='checkbox' onClick={checkboxClick}/>},
-      {"id":1, "parentId":0 ,"name":"External Accounting", "account":"" ,"checkbox":<input className="selection" id="1" type='checkbox' onClick={checkboxClick}/>},
-      {"id":2, "parentId":1 ,"name":"Cash and Cash balances", "car":"", "city":"","gender":"" ,"checkbox":<input className="selection" id="2" type='checkbox' onClick={checkboxClick}/> },
-      {"id":3, "parentId":2 ,"name":"Cash Reserve", "account":1010131,"created_on":"30.8.2020","created_by":"SAP" ,"currency":"CHF" ,"checkbox":<input className="selection" id="3" type='checkbox' onClick={checkboxClick}/>},
-      {"id":4, "parentId":2 ,"name":"Values Undergoing Collection", "account":1012131 ,"created_on":"25.8.2020","created_by":"SAP","currency":"CHF" ,"checkbox":<input className="selection" id="4" type='checkbox' onClick={checkboxClick}/>},
-      {"id":5, "parentId":2 ,"name":"Bank Account Number", "account":1011000,"created_on":"22.8.2020","created_by":"SAP","currency":"EUR" ,"checkbox":<input className="selection" id="5" type='checkbox' onClick={checkboxClick}/>},
-      {"id":6, "parentId":2 ,"name":"Bank Clearing Number", "account":1011100 ,"created_on":"14.8.2020","created_by":"SAP","currency":"CHF" ,"checkbox":<input className="selection" id="6" type='checkbox' onClick={checkboxClick}/>},
-      {"id":7, "parentId":2 ,"name":"IFX Adjustment Account: FI-GL",  "account":1011200,"created_on":"28.8.2020","created_by":"SAP","currency":"EUR" ,"checkbox":<input className="selection" id="7" type='checkbox' onClick={checkboxClick}/> },
-      {"id":8, "parentId":1 ,"name":"Loans and Advances",  "account":"" ,"checkbox":<input className="selection" id="8" type='checkbox' onClick={checkboxClick}/>},
-      {"id":9, "parentId":8 ,"name":"Risk Provision", "account":"" ,"checkbox":<input className="selection" id="9" type='checkbox' onClick={checkboxClick}/>},
-      {"id":10, "parentId":1 ,"name":"Derivative Hedging Instruments - Assets", "account":"" ,"checkbox":<input className="selection" id="10" type='checkbox' onClick={checkboxClick}/>},
-      {"id":11, "parentId":10 ,"name":"Derivative Hedge Accounting",  "account":1040111,"created_on":"21.8.2020","created_by":"SAP" ,"currency":"USD" ,"checkbox":<input className="selection" id="11" type='checkbox' onClick={checkboxClick}/>},
-      {"id":12, "parentId":10 ,"name":"Fair Value Charges of Hedge Items",  "account":1040211,"created_on":"10.8.2020","created_by":"SAP","currency":"USD" ,"checkbox":<input className="selection" id="12" type='checkbox' onClick={checkboxClick}/>},
-      {"id":13, "parentId":1 ,"name":"Securities and Equity Holding",  "account":"" ,"checkbox":<input className="selection" id="13" type='checkbox' onClick={checkboxClick}/>},
-      {"id":14, "parentId":13 ,"name":"Shares of Corporation",  "account":"" ,"checkbox":<input className="selection" id="14" type='checkbox' onClick={checkboxClick}/>},
-      {"id":15, "parentId":1 ,"name":"Trading Assets",  "account":"" ,"checkbox":<input className="selection" id="15" type='checkbox' onClick={checkboxClick}/>},
-      {"id":16, "parentId":1 ,"name":"Intangible Assets",  "account":"" ,"checkbox":<input className="selection" id="16" type='checkbox' onClick={checkboxClick}/>},
-      {"id":17, "parentId":16 ,"name":"Patents",  "account":"" ,"checkbox":<input className="selection" id="17" type='checkbox' onClick={checkboxClick}/>},
-      {"id":18, "parentId":16 ,"name":"Trade Marks",  "account":"" ,"checkbox":<input className="selection" id="18" type='checkbox' onClick={checkboxClick}/>},
-      {"id":19, "parentId":1 ,"name":"Fixed Assets",  "account":"" ,"checkbox":<input className="selection" id="19" type='checkbox' onClick={checkboxClick}/>},
-      {"id":20, "parentId":19 ,"name":"Property & Equipments",  "account":1030255,"created_on":"5.8.2020","created_by":"SAP","currency":"EUR" ,"checkbox":<input className="selection" id="20" type='checkbox' onClick={checkboxClick}/>},
-      {"id":21, "parentId":19 ,"name":"Cash",  "account":1030544,"created_on":"13.8.2020","created_by":"SAP","currency":"CHF" ,"checkbox":<input className="selection" id="21" type='checkbox' onClick={checkboxClick}/>},
-      {"id":22, "parentId":1 ,"name":"Income Tax Receivables",  "account":"","created_on":"18.8.2020","created_by":"SAP","currency":"EUR" ,"checkbox":<input className="selection" id="22" type='checkbox' onClick={checkboxClick}/>},
-      {"id":23, "parentId":22 ,"name":"Tax Return",  "account":1010113,"created_on":"20.8.2020","created_by":"SAP","currency":"USD" ,"checkbox":<input className="selection" id="23" type='checkbox' onClick={checkboxClick}/>},
-      {"id":24, "parentId":1 ,"name":"Other Assets",  "account":"" ,"checkbox":<input className="selection" id="24" type='checkbox' onClick={checkboxClick}/>},
-      {"id":25, "parentId":23 ,"name":"Investments",  "account":1050466,"created_on":"19.8.2020","created_by":"SAP","currency":"USD" ,"checkbox":<input className="selection" id="25" type='checkbox' onClick={checkboxClick}/>}    
-  ]
-  
-  );
+  const [rows, setRows] = useState([]);
 
   useEffect(() => {
     const apiUrl = 'https://raw.githubusercontent.com/anilkumar9854/treetabledata/main/treeData.json';
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        //setRows(data);
+        setRows(data);
 
       }, []);
   }, []);
@@ -183,10 +156,11 @@ export default () => {
 
   const [tableColumnExtensions] = useState([{
     columnName: 'name',
-    width: 450
+    width: 430,
   },{
     columnName: 'checkbox',
-    width: 30
+    width: 40,
+    textAlign: "center"
   } ]);
   const [defaultExpandedRowIds] = useState([0, 1, 2, 3, 4, 5, 6]);
   const onSumbnit = () => {
@@ -311,7 +285,18 @@ export default () => {
               showSelectAll />
               <TableInlineCellEditing /> 
               <TableEditColumn width = {60} showDeleteCommand commandComponent = { Command }/> 
-
+              
+              <Getter
+                name="tableColumns"
+                computed={({ tableColumns }) => {
+                  const result = [
+                    ...tableColumns.filter(c => c.type !== TableEditColumn.COLUMN_TYPE),
+                    { key: 'editCommand', type: TableEditColumn.COLUMN_TYPE, width: 40 }
+                  ];
+                  return result;
+                }
+                }
+              />
             {/* <PagingPanel /> */ }
             </Grid>
           </Paper>
